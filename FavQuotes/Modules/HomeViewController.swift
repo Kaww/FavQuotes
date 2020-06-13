0d1447d232ec42941e1e2c1998fdae8e8f102bd9
Kaww <kawrantin.le-goff@epitech.eu>
2020-06-13 18:24:00 +0800

    sources

---
+++ FavQuotes/Modules/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FavQuotes
+//
+//  Created by kaww on 13/06/2020.
+//  Copyright © 2020 Kaww. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Quotes"
+    }
+
+
+}
+
